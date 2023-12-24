dc5ca07d5f31ce2ade9a5cc026452f8c5829e60a
hayala124 <elnur7803@gmail.com>
2023-12-25 00:54:28 +0300

    programExam
    программа для Платёжного поручения Разработка программных модулей, подготовка к экзамену

---
+++ ekzamen/Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace ekzamen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Chartreuse;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(195, 355);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Отправить";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.HotPink;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Location = new System.Drawing.Point(454, 355);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 35);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Отмена";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(3, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 22);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Номер реквизита";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(408, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(166, 22);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Дата заполнения: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(191, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label1";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(618, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 22);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label2";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(3, 74);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 22);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Плательщик";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(3, 108);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 22);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Сумма";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(3, 146);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(167, 22);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Банк плательщика";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(3, 195);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(47, 22);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "БИК";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(408, 71);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(49, 22);
+            this.label9.TabIndex = 4;
+            this.label9.Text = "ИНН";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(408, 146);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(153, 22);
+            this.label10.TabIndex = 4;
+            this.label10.Text = "Банк получателя";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11.Location = new System.Drawing.Point(408, 190);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(153, 22);
+            this.label11.TabIndex = 4;
+            this.label11.Text = "Счёт получателя";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(195, 71);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(197, 28);
+            this.textBox1.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "ПАО «Лучший банк»",
+            "ПАО «Главный банк»",
+            "ПАО «Замечательный банк»"});
+            this.comboBox1.Location = new System.Drawing.Point(195, 149);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(197, 30);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(195, 190);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(197, 28);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox4.Location = new System.Drawing.Point(592, 68);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(197, 28);
+            this.textBox4.TabIndex = 6;
+            this.textBox4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox4_KeyPress);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox6.Location = new System.Drawing.Point(592, 190);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(197, 28);
+            this.textBox6.TabIndex = 6;
+            this.textBox6.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox6_KeyPress);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "ПАО «Лучший банк»",
+            "ПАО «Главный банк»",
+            "ПАО «Замечательный банк»"});
+            this.comboBox2.Location = new System.Drawing.Point(591, 146);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(197, 30);
+            this.comboBox2.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(195, 111);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(197, 28);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label12.Location = new System.Drawing.Point(587, 108);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(58, 22);
+            this.label12.TabIndex = 3;
+            this.label12.Text = "label2";
+            this.label12.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Платёжное поручение";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label12;
+    }
+}
+
---
+++ ekzamen/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ekzamen
+{
+    public partial class Form1 : Form
+    {
+        static int count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void proverka(int number, string Name, string text)
+        {
+            if (text.Length < number)
+            {
+                MessageBox.Show($"Введите {number} цифр в {Name}", "Ошибка");
+                return;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Если кол-во введенных цифр в БИК меньше 9, то выводится сообщение об ошибке.
+            proverka(9, "БИК", textBox3.Text);
+
+            // Если кол-во введенных цифр в ИНН меньше 10 или равно 11, то выводится сообщение об ошибке.
+            if (textBox4.Text.Length < 10 || textBox4.Text.Length == 11)
+            {
+                MessageBox.Show("Введите 10 или 12 цифр в ИНН!", "Ошибка");
+                return;
+            }
+
+            // Если кол-во введенных цифр в Счёте получателя меньше 20, то выводится сообщение об ошибке.
+            proverka(20, "Счёте получателя", textBox6.Text);
+
+            // Если не все поля заполнены, выводится ошибка. 
+            if ((textBox1.Text == "") || (textBox2.Text == "") || (comboBox1.Text == "") || (textBox3.Text == "") ||
+                (textBox4.Text == "") || (comboBox2.Text == "") || (textBox6.Text == ""))
+                MessageBox.Show("Заполните все поля!", "Ошибка");
+            // Если все поля заполнены, то необходимо подтвердить действие для отправки данных.
+            else
+            {
+                DialogResult result = MessageBox.Show("Отправить?", "Подтверждение отправки", MessageBoxButtons.OKCancel);
+                if (result == DialogResult.OK)
+                {
+                    label3.Visible = true;
+                    label4.Visible = true;
+                    label12.Visible = true;
+                    label3.Text = (++count).ToString();
+                    label4.Text = DateTime.Now.ToString();
+
+                    if (textBox4.Text.Length == 10)
+                        label12.Text = "Юридическое лицо";
+                    else if (textBox4.Text.Length == 12)
+                        label12.Text = "Предприниматель";
+                    foreach (Control control in this.Controls)
+                    {
+                        if ((control is TextBox))
+                        {
+                           
+                            TextBox textBox = control as TextBox;
+                            //textBox.ReadOnly = true;
+                            comboBox1.Enabled = false;
+                            comboBox2.Enabled = false;
+                            textBox.Enabled = false;
+                            button1.Enabled = false;
+                        }
+                    }
+                }
+            }
+        }
+
+        // Проверка на ввод нецифрового значения для БИК.
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (textBox3.Text.Length < 9 && char.IsDigit(e.KeyChar))
+            {
+                return;
+            }
+            e.Handled = true;
+        }
+
+        // Проверка на ввод нецифрового значения для Суммы.
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Преобразование символа запятой в точку.
+            if (e.KeyChar == ',')
+            {
+                e.KeyChar = '.';
+            }
+
+            // Ограничение ввода символов.
+            if (e.KeyChar < '0' | e.KeyChar > '9' && e.KeyChar != (char)Keys.Back && e.KeyChar != '.')
+            {
+                e.Handled = true;
+            }
+            // Запрет ввода разделителя первым символом.
+            if (textBox2.SelectionStart == 0 & e.KeyChar == '.')
+            {
+                e.Handled = true;
+            }
+            // Запрет ввода числа при вводе первым символом 0. 
+            if (textBox2.Text == "0")
+            {
+                if (e.KeyChar != '.' & e.KeyChar != (char)Keys.Back)
+                {
+                    e.Handled = true;
+                }
+            }
+            // Запрет на ввод более 2 знаков после точки.
+            if (textBox2.Text.IndexOf('.') > 0)
+            {
+                if (textBox2.Text.Substring(textBox2.Text.IndexOf('.')).Length > 2)
+                {
+                    if (e.KeyChar != (char)Keys.Back)
+                    {
+                        e.Handled = true;
+                    }
+                }
+            }
+            // Ввод только 1 разделителя.
+            if (e.KeyChar == '.')
+            {
+                if (textBox2.Text.IndexOf('.') != -1)
+                {
+                    e.Handled = true;
+                }
+            }
+
+        }
+
+        private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (textBox4.Text.Length < 12 && char.IsDigit(e.KeyChar))
+            {
+                return;
+            }
+            e.Handled = true;
+
+        }
+
+        private void textBox6_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (textBox6.Text.Length < 20 && char.IsDigit(e.KeyChar))
+            {
+                return;
+            }
+            e.Handled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Отменить?", "Подтверждение отмены", MessageBoxButtons.OKCancel);
+            if (result == DialogResult.OK)
+            {
+                foreach (Control control in this.Controls)
+                {
+                    if ((control is TextBox))
+                    {
+                        TextBox textBox = control as TextBox;
+                        textBox.Clear();
+                        label3.Text = "";
+                        label4.Text = string.Empty;
+                        label12.Text = "";
+                        comboBox1.SelectedIndex = -1;
+                        comboBox2.SelectedIndex = -1;
+                       
+                        comboBox1.Enabled = true;
+                        comboBox2.Enabled = true;
+                        textBox.Enabled = true;
+                        button1.Enabled = true;
+                    }
+                }
+            }
+        }
+    }
+}
